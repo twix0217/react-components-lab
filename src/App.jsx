bfc9db3054d54fbb4ba48d78d39e51194da6ced9
twix217 <hussainbarakat02@gmail.com>
2024-08-05 16:20:37 +0300

    finished

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+// src/App.jsx
+import './App.css';
+import WeatherForecasts from './components/WeatherForecasts/WeatherForecasts.jsx';
+
+const weatherForecasts = [
+  {
+    day: 'Mon',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg',
+    imgAlt: 'sun icon',
+    conditions: 'sunny',
+    time: 'Morning',
+  },
+  {
+    day: 'Tue',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg',
+    imgAlt: 'moon icon',
+    conditions: 'clear',
+    time: 'Night',
+  },
+  {
+    day: 'Wed',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg',
+    imgAlt: 'clouds with lightning icon',
+    conditions: 'stormy',
+    time: 'All Day',
+  },
+  {
+    day: 'Thu',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg',
+    imgAlt: 'sun overcast by clouds icon',
+    conditions: 'overcast',
+    time: 'Evening',
+  },
+  {
+    day: 'Fri',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg',
+    imgAlt: 'moon overcast by clouds icon',
+    conditions: 'cloudy',
+    time: 'Night',
+  },
+];
+
+const App = () => {
+
+  return (
+    <>
+    <h1>Local Weather</h1>
+    <section>
+    <ul>
+      {weatherForecasts.map((weather) => (
+          <WeatherForecasts key={weather.id} weather={weather} />
+        ))}
+      </ul>
+    </section>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/WeatherForecasts/WeatherForecasts.jsx
@@ -0,0 +1,15 @@
+const WeatherForecasts = (props) => {
+    const { weather} = props
+    return (
+      
+        <div className="weather">
+  <h2>{weather.day} </h2>
+  <img src={weather.img}  alt={weather.imgAlt} />
+  <p><span>conditions: </span>{weather.conditions} </p>
+  <p><span>time: </span> {weather.time} </p>
+</div>
+     
+    );
+  };
+  
+  export default WeatherForecasts;
